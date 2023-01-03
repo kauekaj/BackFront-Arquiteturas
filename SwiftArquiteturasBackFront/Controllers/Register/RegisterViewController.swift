ddf758dd73de273df0bb5e287c19bc5f58e8f1b6
Kaue Jacyntho <kauekaj@gmail.com>
2023-01-03 19:51:27 -0300

    Implement RegisterPresenter and RegisterViewController

--- SwiftArquiteturasBackFront/Controllers/Register/RegisterViewController.swift
+++ SwiftArquiteturasBackFront/Controllers/Register/RegisterViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    let presenter = RegisterPresenter()
     
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
@@ -17,6 +18,7 @@ class RegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.presenter.delegate = self
     }
     
     
@@ -34,33 +36,28 @@ class RegisterViewController: UIViewController {
             if password != confirmPassword {
                 self.showMessage(title: "Erro", message: "As senhas não conferem")
                 return
-                
             }
             
+            let userModel = UserModel(email: email, password: password)
             
-            let manager = UserManager(business: UserBusiness())
-            
-            manager.register(email: email,
-                             password: password) { userModel in
-                self.openHomeView()
-            } failureHandler: { error in
-                self.showMessage(title: "Erro", message: error?.localizedDescription ?? "")
-            }
+            presenter.register(userModel: userModel)
         }
-        
+    }
+}
+
+extension RegisterViewController: RegisterPresenterDelegate {
+    
+    func showMessage(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        self.present(alert, animated: true)
     }
     
-    func openHomeView() {
+    func goHome() {
         let homeView = self.storyboard?.instantiateViewController(withIdentifier: "HomeViewController") as! HomeViewController
         homeView.modalPresentationStyle = .fullScreen
         self.present(homeView, animated: true)
     }
     
-    func showMessage(title: String, message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        
-        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-        
-        self.present(alert, animated: true)
-    }
+    
 }
