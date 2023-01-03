d668c7cbe2a565ace3252f2624459cb430690d88
Kaue Jacyntho <kauekaj@gmail.com>
2023-01-03 19:22:00 -0300

    Implement LoginPresenter and LoginViewcontroller

---
+++ SwiftArquiteturasBackFront/Controllers/Login/LoginPresenter.swift
@@ -0,0 +1,42 @@
+//
+//  LoginPresenter.swift
+//  SwiftArquiteturasBackFront
+//
+//  Created by Kaue de Assis Jacyntho on 03/01/23.
+//
+
+import Foundation
+
+protocol LoginPresenterDelegate {
+    func showMessage(title: String, message: String)
+    func goHome()
+}
+
+class LoginPresenter {
+    
+    var delegate: LoginPresenterDelegate?
+    
+    func login(userModel: UserModel) {
+        let manager = UserManager(business: UserBusiness())
+        manager.login(email: userModel.email,
+                      password: userModel.password) { userModel in
+            self.goHome()
+        } failureHandler: { [weak self] error in
+            self?.delegate?.showMessage(title: "Erro", message: error?.localizedDescription ?? "Nao foi possível identificar o erro.")
+        }
+        
+    }
+    
+    func goHome() {
+        self.delegate?.goHome()
+    }
+    
+}
+
+extension LoginPresenter: LoginPresenterDelegate {
+    func showMessage(title: String, message: String) {
+        
+    }
+    
+    
+}
